b4b94a3600b219787ecfc39d240637a537aaec05
Tejeshgowd0986 <102064585+tejeshgow@users.noreply.github.com>
2024-07-16 12:05:37 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -53,7 +53,7 @@ TEST(StringCalculatorAddTests, ExpectSumWithCustomDelimiterAtStart) {
  
 // Test case for handling negative numbers
 TEST(StringCalculatorAddTests, ExpectExceptionForNegativeNumbers) {
-    const char* input = "1,-3,-4";
+    const char* input = "-2,3,-4";
      add(input);
 }
  
